577fc67f158022cb2e0ea73f964f2fac2a1b857e
ennain <inenna90@gmail.com>
2026-03-02 10:38:00 +0700

    user/create

--- index.php
+++ index.php
@@ -3,15 +3,17 @@ ob_start();
 require_once './init/init.php';
 
 $user = loggedInUser();
+$isAdmin = isAdmin();  
 
 include './includes/header.inc.php';
 include './includes/navbar.inc.php';
 
 
 
-$available_pages = ['login', 'register', 'dashboard', 'logout', 'profile'];
+$available_pages = ['login', 'register', 'dashboard', 'logout', 'profile','user/create','user/list'];
 $logged_in_pages = ['dashboard', 'profile'];
 $non_logged_in_pages = ['login', 'register'];
+$admin_pages = ['user/create','user/list'];
 
 $page = '';
 if (isset($_GET['page'])) {
@@ -24,6 +26,11 @@ if (in_array($page, $logged_in_pages) && empty($user)) {
 
 if (in_array($page, $non_logged_in_pages) && !empty($user)) {
     header('Location: ./?page=dashboard');
+}
+if (in_array($page, $available_pages) && !empty($user)) {
+    if(in_array($page, $admin_pages) && !$isAdmin) {
+        header('Location: ./?page=dashboard');
+    }
 }
 
 if (in_array($page, $available_pages)) {
--- init/init.php
+++ init/init.php
@@ -5,7 +5,7 @@ $baseUrl = '/G19BCSY3A/';
 session_start();
 require_once './init/db.init.php';
 require_once './init/func/auth.func.php';
-
+require_once './init/func/user.func.php';
 
 
 ?>
